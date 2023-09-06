168b5919aea2008ac30c8d4273d04be12d833125
Cisco <ciscophrenzy@gmail.com>
2023-09-06 14:56:24 -0600

    Fix linters

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Splashscreen from './components/Splashscreen';
+// import { Routes, Route } from 'react-router-dom';
+// import Splashscreen from './components/Splashscreen';
+import logo from './logo.svg';
 import './App.scss';
 
 function App() {
@@ -9,7 +10,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js
+            </code>
+             and save to reload.
         </p>
         <a
           className="App-link"
--- src/Components/index.js
+++ src/Components/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import store from './redux/store';
+import store from '../redux/store';
 import App from '../App';
 import './index.scss';
 
-ReactDOM.render(
+createRoot(
   <Provider store={store}>
     <Router>
       <App />
