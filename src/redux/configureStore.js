5c76ce2030cfa72878bd2fb8162e3d0210dce3fc
naanmohammed <naanahmed24@gmail.com>
2023-09-07 23:55:33 +0300

    Update config files

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -12,6 +12,7 @@ import motorSlice from './motorcycles/motorSlice';
 import reservationSlice from './reservations/reservationSlice';
 import newmotorSlice from './motorcycle/motorcycleSlice';
 import delresReducer from './reservations/delresSlice';
+import motorcyclesReducer from './motorcycles/mtrs'
 
 const logger = createLogger();
 
@@ -29,6 +30,7 @@ const store = configureStore({
     motor: motorSlice,
     reservation: reservationSlice,
     myreservation: delresReducer,
+    motorcycles: motorcyclesReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(findNonSerializableValue).concat(logger), // eslint-disable-line
 });
